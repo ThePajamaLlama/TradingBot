62fa6ed71c12c934036dd478eafb1b1e72ea9b8e
Yaritza Rodriguez <ThePajamaLlama>
2021-04-16 10:56:07 -0700

    small updates

--- kucoinKeys.py
+++ kucoinKeys.py
@@ -1,10 +1,11 @@
 real_keys = {
-    'apiKey' = '6079cc0647587100066cf76a'
-    'apiSecret' = 'f6bd077e-ba5e-45f1-9559-209095c4e210'
-    'apiPassphrase'  = 'passfraze'
+    'apiKey' : '6079cc0647587100066cf76a',
+    'apiSecret' : 'f6bd077e-ba5e-45f1-9559-209095c4e210',
+    'apiPassphrase'  : 'passfraze'
 }
 
 sandbox_keys = {
-    'apiKey' = 'apiKey'
-    'apiSecret' = 'apiSecret'
+    'apiKey' : '6079cf0f365ac6000689ea5f',
+    'apiSecret' : '05ccc08e-cec4-40ab-966c-648d0c04cff2',
+    'apiPassphrase' : 'passfraze'
 }
--- trader.py
+++ trader.py
@@ -1,3 +1,13 @@
 #Kucoin trading bot
+import sys
+sys.path.append('C:\\Users\\zinex\\Documents\\Python\\Trader')
+from kucoinKeys import sandbox_keys as keys
 
-#New comment
+apiKey = keys['apiKey']
+apiSecret = keys['apiSecret']
+passphrase = keys['apiPassphrase']
+
+from kucoin.client import User 
+client = User(apiKey, apiSecret, apiPassphrase, is_sandbox=True)
+
+print(client)
