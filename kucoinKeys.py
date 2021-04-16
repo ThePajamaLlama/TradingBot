de8470bbe11a318f71c6b8aeb94bcf4e0af09eb2
Yaritza Rodriguez <ThePajamaLlama>
2021-04-16 10:41:09 -0700

    updated keys

--- kucoinKeys.py
+++ kucoinKeys.py
@@ -1,4 +1,10 @@
 real_keys = {
+    'apiKey' = '6079cc0647587100066cf76a'
+    'apiSecret' = 'f6bd077e-ba5e-45f1-9559-209095c4e210'
+    'apiPassphrase'  = 'passfraze'
+}
+
+sandbox_keys = {
     'apiKey' = 'apiKey'
     'apiSecret' = 'apiSecret'
 }
